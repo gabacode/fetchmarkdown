34d13739af857275d591c8acebe7f173ba1fc5c9
Gabriele Scalici <gaba41@gmail.com>
2021-08-02 17:57:57 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,46 @@
-import * as React from "react"
+import React, {Fragment} from "react"
+import { graphql, Link } from "gatsby"
 
-const IndexPage = () => {
+export default function IndexPage({data}) {
+  const postList = data.allMdx.edges
   return (
     <main>
-      <title>Home Page</title>
-      Index
+      <title>
+        Home Page
+      </title>
+      <ul>
+      {
+        postList.map((post, index) => (
+          <Fragment>
+            <li key={index}>
+                <h1 style={{display:'inline'}}>{post.node.frontmatter.title}</h1>
+                <span> (by {post.node.frontmatter.author})</span><br/>
+                <Link to={`posts${post.node.fields.slug}`}>{post.node.fields.slug}</Link>
+            </li>
+          </Fragment>
+        ))
+      }
+      </ul>
     </main>
   )
 }
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMdx {
+      edges {
+          node {
+              body
+              fields {
+                  slug
+              }
+              frontmatter {
+                title
+                author
+                hasBool
+              }
+          }
+      }
+    }
+  }
+`
