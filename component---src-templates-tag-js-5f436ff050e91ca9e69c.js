d63f985463731fcc38ce0e8ab63af8c0b30a5d4d
Gabriele Scalici <gaba41@gmail.com>
2021-08-04 22:49:22 +0200

    Updates

---
+++ component---src-templates-tag-js-5f436ff050e91ca9e69c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfetchmarkdown=self.webpackChunkfetchmarkdown||[]).push([[969],{5847:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r=t(7294),a=t(5127);function u(e){var n=e.data.allMdx.edges;return r.createElement(r.Fragment,null,r.createElement(a.Z,null,n.map((function(e,n){return r.createElement("div",{key:n},JSON.stringify(e.node.slug))}))))}}}]);
+//# sourceMappingURL=component---src-templates-tag-js-5f436ff050e91ca9e69c.js.map
